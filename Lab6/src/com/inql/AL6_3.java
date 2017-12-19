64c7077bbffc48e4eca2017177fc6780a879caa4
inql <dbinkus96@gmail.com>
2017-12-19 18:28:33 +0100

    prefinal commit

--- Lab6/src/com/inql/AL6_3.java
+++ Lab6/src/com/inql/AL6_3.java
@@ -13,9 +13,11 @@ public class AL6_3 {
     private Surname[] table;
     private final Surname del = new Surname(0,"DEL");
     int arraySize;
+    int numberOfAttempts;
 
     public AL6_3(String path) throws IOException {
         this.surnames = readFile(path);
+        this.numberOfAttempts=0;
     }
 
     public Surname[] getSurnames() {
@@ -61,6 +63,7 @@ public class AL6_3 {
             if(doTests)System.out.println("Wydruk kontrolny: \n\ti="+i+"\n\tdługość tablicy: "+table.length+"\n\twartość słowa jako long: "+word+"\n\tpo hashowaniu: "+j);
             if(table[j]==null || table[j].getSurname().equals("DEL")){
                 table[j] = surname;
+                numberOfAttempts+=(i+1);
                 if(doTests) System.out.println("Wartość została umieszona na miejscu: "+j);
                 return;
             }
@@ -71,7 +74,7 @@ public class AL6_3 {
     }
 
     private int hashSearch(String surname, HashFunction hashFunction, boolean doTests){
-        int i =10;
+        int i =0;
         int j =0;
         long word = Utilities.wordToLong(surname,111);
         if(doTests) System.out.println("Szukanie nazwiska: "+surname);
@@ -81,6 +84,7 @@ public class AL6_3 {
             try{
                 if (table[j].getSurname().equals(surname)){
                     if(doTests)System.out.println("Znaleziono wartość na pozycji: "+j);
+                    numberOfAttempts+=(i+1);
                     return j;
                 }
             }catch(NullPointerException e){ }
@@ -107,10 +111,16 @@ public class AL6_3 {
     //testy na małej tablicy
 
     public void run(){
-        arraySize=29;
-        doTest(arraySize,(k, m, i) -> (int) ((k % m + i)%m),"\nAdresowanie otwarte liniowe\nFunkcja hashująca: (k, m, i) ->  ((k % m +i)%m)");
-        doTest(arraySize,(k, m, i) -> (int) ((k % m + (i*i))%m),"\nAdresowanie otwarte kwadratowe\nFunkcja hashująca: (k, m, i) ->  ((k % m +i^2)%m)");
-        doTest(arraySize,(k, m, i) -> (int) ((k % m + i*(1+(k%(m-2))))%m),"\nAdresowanie otwarte kwadratowe\nFunkcja hashująca: (k, m, i) ->  ((k % m + i*(1+(k%(m-2))))%m)");
+        arraySize=2039;
+        //testowanie działania funkcji
+//        doTest(arraySize,(k, m, i) -> (int) ((k % m + i)%m),"\nAdresowanie otwarte liniowe\nFunkcja hashująca: (k, m, i) ->  ((k % m +i)%m)");
+//        doTest(arraySize,(k, m, i) -> (int) ((k % m + (i*i))%m),"\nAdresowanie otwarte kwadratowe\nFunkcja hashująca: (k, m, i) ->  ((k % m +i^2)%m)");
+//        doTest(arraySize,(k, m, i) -> (int) ((k % m + i*(1+(k%(m-2))))%m),"\nAdresowanie otwarte kwadratowe\nFunkcja hashująca: (k, m, i) ->  ((k % m + i*(1+(k%(m-2))))%m)");
+
+        //pomiary
+        arraySize=2039;
+        doEfficiencyTest(arraySize,(k, m, i) -> (int) ((k % m + i)%m),"\nAdresowanie otwarte liniowe\nFunkcja hashująca: (k, m, i) ->  ((k % m +i)%m)");
+
 
     }
 
@@ -140,7 +150,83 @@ public class AL6_3 {
         hashDelete("Bińkuś",hashFunction,true);
     }
 
+    public void doEfficiencyTest(int arraySize, HashFunction hashFunction, String message){
+        System.out.println(Utilities.repeat("-",40)+"\nTesty pomiarowe, arraySize: "+arraySize);
+        setTable(new Surname[arraySize]);
+        //wypełnienie tablicy do 50%
+        insertTest(hashFunction,0.5);
+        clearTable();
+        //wypełnienie tablicy do 70%
+        insertTest(hashFunction, 0.7);
+        clearTable();
+        insertTest(hashFunction, 0.9);
+        clearTable();
+        //pomiar szukania
+        int i;
+        for(i = 0; i<(int)(arraySize*0.8); i++){
+            hashInsert(surnames[i],hashFunction,false);
+        }
+        int range = i-20;
+        while(i>=range){
+            i--;
+            searchTest(hashFunction,i);
+        }
+        //pomiar usuwania
+        clearTable();
+        for(i = 0; i<(int)(arraySize*0.8); i++){
+            hashInsert(surnames[i],hashFunction,false);
+        }
+        range = i/2;
 
+        while(i>=range){
+            i--;
+            hashDelete(surnames[i].getSurname(),hashFunction,false);
+        }
+        int placesTaken = 0;
+        for(Surname surname : table){
+            if(surname!=null && surname != del){
+                placesTaken++;
+            }
+//            System.out.println(surname);
+        }
+//        System.out.println(placesTaken);
+        for(i=placesTaken; i<(int)(arraySize*0.8);i++){
+            hashInsert(surnames[i+range],hashFunction,false);
+        }
+        int delCount = 0;
+        for(Surname surname : table){
+            if(surname==del)
+                delCount++;
+//            System.out.println(surname);
+        }
+        System.out.println("Ilość wartości DEL: "+delCount);
+
+
+
+    }
+
+    private void insertTest(HashFunction hashFunction, double perc){
+        int numberOfWordsToInsert = (int)(arraySize * perc);
+        for(int i =0; i<numberOfWordsToInsert; i++){
+            hashInsert(surnames[i],hashFunction,false);
+        }
+        double avg = (double)numberOfAttempts/numberOfWordsToInsert;
+        System.out.println("\nIlość słów do wstawienia: "+numberOfWordsToInsert+"\nIlość prób: "+numberOfAttempts+"\nŚrednia: "+avg);
+        numberOfAttempts=0;
+    }
+
+    private void searchTest(HashFunction hashFunction, int index){
+        System.out.println("Szukam..."+surnames[index]);
+        hashSearch(surnames[index].getSurname(),hashFunction,false);
+        System.out.println("Ilość prób: "+numberOfAttempts);
+        numberOfAttempts=0;
+    }
+
+    private void clearTable(){
+        for(int i = 0; i<table.length; i++){
+            table[i] = null;
+        }
+    }
 
 
 
